0814253ffe9dd02013e18b97994250f0f6bed43f
Patrik Örnebjär <patrikbj@gmail.com>
2024-01-13 17:20:07 +0100

    Initial commit

---
+++ src/main/resources/archetype-resources/src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+package $package;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MainTest {
+    @Test
+    void test() {
+        assertThat(true).isTrue();
+    }
+}
